8066d510fb07c914c49b3a8b2a33b02c7af750da
sergeantfox234 <nathanafox.us@gmail.com>
2024-02-17 21:58:58 -0500

    Fixed home page

--- src/app/language-wheel/language-wheel.component.ts
+++ src/app/language-wheel/language-wheel.component.ts
@@ -1,13 +1,41 @@
 import { Component, Input } from '@angular/core';
 import { GithubLanguage } from '../github-languages';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-language-wheel',
   standalone: true,
-  imports: [],
+  imports: [      
+    CanvasJSAngularChartsModule
+  ],
   templateUrl: './language-wheel.component.html',
   styleUrl: './language-wheel.component.scss'
 })
 export class LanguageWheelComponent {
   @Input() languages:GithubLanguage[] = [];
+
+  get chartOptions() {
+    return {
+      title: {
+        text: "Languages",
+      },
+      theme: "light2",
+      legend: {
+        maxWidth: 350,
+        itemWidth: 120,
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        indexLabel: "{name}: {y}",
+        yValueFormatString: "#,###.##'%'",
+        dataPoints: this.languages.map((language) => {
+          return {
+            name: language.name, 
+            y: 100 * (Math.round((language.count + Number.EPSILON) * 10000) / 10000)
+          };
+        })
+      }]
+    };
+  }
 }

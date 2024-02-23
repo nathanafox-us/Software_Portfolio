8d2ceec0aff9cd861636670432ecab6e4886e762
sergeantfox234 <nathanafox.us@gmail.com>
2024-02-22 23:00:01 -0500

    ui updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MissionPageComponent } from './mission-page/mission-page.component';
 import { InquiriesPageComponent } from './inquiries-page/inquiries-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
-    { path: 'mission', component: MissionPageComponent},
     { path: 'inquiries', component: InquiriesPageComponent},
-    { path: 'products', component: ProductsPageComponent}
 ];
--- src/app/tab-bar/tab-bar.component.ts
+++ src/app/tab-bar/tab-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-tab-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.scss'
 })

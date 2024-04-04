cf3f270c98f8f189386bd4956293b969bfcc65ed
sergeantfox234 <nathanafox.us@gmail.com>
2024-04-04 08:26:55 -0400

    Improved UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InquiriesPageComponent } from './inquiries-page/inquiries-page.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
     { path: 'inquiries', component: InquiriesPageComponent},
-    { path: 'blog', component: HomeComponent},
+    { path: 'blog', component: BlogComponent},
 ];
